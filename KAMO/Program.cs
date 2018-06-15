701319b319287869232b2c2e2720ee4b1a3a024f
Max Bondarchuk <bondarchuk.m.y@gmail.com>
2018-06-15 22:38:51 +0300

    New case not accepted

--- KAMO/Program.cs
+++ KAMO/Program.cs
@@ -7,14 +7,19 @@ namespace KAMO
     {
         private static void Main()
         {
-            var wm = new WholeMatrix(10, 8, 6);
+            var wm = new WholeMatrix(size: 27, i: 396, j: 384);
+			//wm.Print();
+			//wm.RotateForwardN(3);
+			wm.PrintDet3();
             Console.WriteLine(wm.Det3());
-            wm.Print();
-            //wm.RotateForwardN(3);
-            wm.Sort();
-            wm.Print();
+			Console.WriteLine();
+			wm.Sort();
+			wm.PrintDet3();
+			Console.WriteLine(wm.Det3());
+			Console.WriteLine();
+			//wm.Print();
 
-            using (var file = new StreamWriter("Matrix.txt"))
+			using (var file = new StreamWriter("Matrix.txt"))
             {
                 file.Write(wm);
             }
--- KAMO/WholeMatrix.cs
+++ KAMO/WholeMatrix.cs
@@ -4,167 +4,196 @@ using System.Text;
 
 namespace KAMO
 {
-    public class WholeMatrix
-    {
-        public int I { get; private set; }
-        public int J { get; private set; }
+	public class WholeMatrix
+	{
+		public int I { get; private set; }
+		public int J { get; private set; }
 
-        private double[,] Matrix { get; }
-        private Circuit Circuit { get; set; }
+		private double[,] Matrix { get; }
+		private Circuit Circuit { get; set; }
 
-        public WholeMatrix(int size, int i, int j)
-        {
-            I = i;
-            J = j;
-            //Matrix = new double[size * 30, size * 30];
-            Matrix = new double[10, 10];
-            FillMatrix();
+		public WholeMatrix(int size, int i, int j)
+		{
+			size *= 25;
 
-            Circuit = new Circuit(i, j, size, CircuitDirection.Clockwise);
-        }
+			I = i;
+			J = j;
+			//Matrix = new double[size * 30, size * 30];
+			Matrix = new double[size, size];
+			FillMatrix();
 
-        public void Print()
-        {
-            for (var i = 0; i < Matrix.GetLength(0); i++)
-            {
-                for (var j = 0; j < Matrix.GetLength(1); j++)
-                    if (Circuit.ContainsPoint(i, j))
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write($"*{Matrix[i, j],-8:N2}");
-                        Console.ForegroundColor = ConsoleColor.Gray;
-                    }
-                    else
-                    {
-                        Console.Write($" {Matrix[i, j],-8:N2}");
-                    }
+			Circuit = new Circuit(i, j, size, CircuitDirection.CounterClockwise);
+		}
 
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-        }
+		public void Print()
+		{
+			for (var i = 0; i < Matrix.GetLength(0); i++)
+			{
+				for (var j = 0; j < Matrix.GetLength(1); j++)
+					if (Circuit.ContainsPoint(i, j))
+					{
+						Console.ForegroundColor = ConsoleColor.Red;
+						Console.Write($"*{Matrix[i, j],-8:N2}");
+						Console.ForegroundColor = ConsoleColor.Gray;
+					}
+					else
+					{
+						Console.Write($" {Matrix[i, j],-8:N2}");
+					}
 
-        public double Det3()
-        {
-            //return 666;
-            var a = Matrix[I - 2, J + 2];
-            var b = Matrix[I - 2, J + 1];
-            var c = Matrix[I - 2, J];
-            var d = Matrix[I - 1, J + 2];
-            var e = Matrix[I - 1, J + 1];
-            var f = Matrix[I - 1, J];
-            var g = Matrix[I, J + 2];
-            var h = Matrix[I, J + 1];
-            var i = Matrix[I, J];
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+		}
 
-            return a * e * i + b * f * g + c * d * h - c * e * g - b * d * i - a * f * h;
-        }
+		public void PrintDet3()
+		{
+			var a = Matrix[I - 2, J - 2];
+			var b = Matrix[I - 2, J - 1];
+			var c = Matrix[I - 2, J];
+			var d = Matrix[I - 1, J - 2];
+			var e = Matrix[I - 1, J - 1];
+			var f = Matrix[I - 1, J];
+			var g = Matrix[I, J - 2];
+			var h = Matrix[I, J - 1];
+			var i = Matrix[I, J];
 
-        public void RotateForwardN(int n)
-        {
-            Console.WriteLine($"Rotating forward {n}");
-            n %= Circuit.Lenth;
-            Console.WriteLine($"Actual rotation = {n}");
+			Console.Write($"{a,-8:N4}");
+			Console.Write($"{b,-8:N4}");
+			Console.Write($"{c,-8:N4}");
+			Console.WriteLine();
+			Console.Write($"{d,-8:N4}");
+			Console.Write($"{e,-8:N4}");
+			Console.Write($"{f,-8:N4}");
+			Console.WriteLine();
+			Console.Write($"{g,-8:N4}");
+			Console.Write($"{h,-8:N4}");
+			Console.Write($"{i,-8:N4}");
+			Console.WriteLine();
+		}
 
-            Circuit.MoveBackward(); // Stay at the end
+		public double Det3()
+		{
+			//return 666;
+			var a = Matrix[I - 2, J - 2];
+			var b = Matrix[I - 2, J - 1];
+			var c = Matrix[I - 2, J];
+			var d = Matrix[I - 1, J - 2];
+			var e = Matrix[I - 1, J - 1];
+			var f = Matrix[I - 1, J];
+			var g = Matrix[I, J - 2];
+			var h = Matrix[I, J - 1];
+			var i = Matrix[I, J];
 
-            var remember = new List<double>();
-            for (var i = Circuit.Lenth - 1; Circuit.Lenth - 1 - n < i; i--, Circuit.MoveBackward())
-            {
-                remember.Add(Matrix[Circuit.I, Circuit.J]);
-            }
-            Circuit.Reset();
-            Circuit.MoveBackward(); // Stay at the end
+			return a * e * i + b * f * g + c * d * h - c * e * g - b * d * i - a * f * h;
+		}
 
-            for (var i = Circuit.Lenth - 1; n <= i; i--, Circuit.MoveBackward())
-            {
-                var previousN = Circuit.GetPreviousN(n);
-                Matrix[Circuit.I, Circuit.J] = Matrix[previousN.Item1, previousN.Item2];
-            }
-            Circuit.Reset();
+		public void RotateForwardN(int n)
+		{
+			Console.WriteLine($"Rotating forward {n}");
+			n %= Circuit.Lenth;
+			Console.WriteLine($"Actual rotation = {n}");
 
-            for (var i = 0; i < n; i++, Circuit.MoveForward())
-            {
-                Matrix[Circuit.I, Circuit.J] = remember[n - 1 - i];
-            }
-            Circuit.Reset();
-        }
+			Circuit.MoveBackward(); // Stay at the end
 
-        public void RotateBackwardN(int n)
-        {
-            Console.WriteLine($"Rotating backward {n}");
-            n %= Circuit.Lenth;
-            Console.WriteLine($"Actual rotation = {n}");
+			var remember = new List<double>();
+			for (var i = Circuit.Lenth - 1; Circuit.Lenth - 1 - n < i; i--, Circuit.MoveBackward())
+			{
+				remember.Add(Matrix[Circuit.I, Circuit.J]);
+			}
+			Circuit.Reset();
+			Circuit.MoveBackward(); // Stay at the end
 
-            var remember = new List<double>();
-            for (var i = 0; i < n; i++, Circuit.MoveForward())
-            {
-                remember.Add(Matrix[Circuit.I, Circuit.J]);
-            }
-            Circuit.Reset();
+			for (var i = Circuit.Lenth - 1; n <= i; i--, Circuit.MoveBackward())
+			{
+				var previousN = Circuit.GetPreviousN(n);
+				Matrix[Circuit.I, Circuit.J] = Matrix[previousN.Item1, previousN.Item2];
+			}
+			Circuit.Reset();
 
-            for (var i = 0; i < Circuit.Lenth - n; i++, Circuit.MoveForward())
-            {
-                var nextN = Circuit.GetNextN(n);
-                Matrix[Circuit.I, Circuit.J] = Matrix[nextN.Item1, nextN.Item2];
-            }
+			for (var i = 0; i < n; i++, Circuit.MoveForward())
+			{
+				Matrix[Circuit.I, Circuit.J] = remember[n - 1 - i];
+			}
+			Circuit.Reset();
+		}
 
-            for (var i = Circuit.Lenth - n; i < Circuit.Lenth; i++, Circuit.MoveForward())
-            {
-                Matrix[Circuit.I, Circuit.J] = remember[i - (Circuit.Lenth - n)];
-            }
-            Circuit.Reset();
-        }
+		public void RotateBackwardN(int n)
+		{
+			Console.WriteLine($"Rotating backward {n}");
+			n %= Circuit.Lenth;
+			Console.WriteLine($"Actual rotation = {n}");
 
-        public void Sort()
-        {
-            bool wasSwap;
-            do
-            {
-                wasSwap = false;
-                for (var i = 0; i < Circuit.Lenth - 1; i++, Circuit.MoveForward())
-                {
-                    var next = Circuit.GetNextN(1);
-                    if (Matrix[Circuit.I, Circuit.J] > Matrix[next.Item1, next.Item2])
-                    {
-                        var tmp = Matrix[Circuit.I, Circuit.J];
-                        Matrix[Circuit.I, Circuit.J] = Matrix[next.Item1, next.Item2];
-                        Matrix[next.Item1, next.Item2] = tmp;
+			var remember = new List<double>();
+			for (var i = 0; i < n; i++, Circuit.MoveForward())
+			{
+				remember.Add(Matrix[Circuit.I, Circuit.J]);
+			}
+			Circuit.Reset();
 
-                        wasSwap = true;
-                    }
-                }
-                Circuit.Reset();
-            } while (wasSwap);
-        }
+			for (var i = 0; i < Circuit.Lenth - n; i++, Circuit.MoveForward())
+			{
+				var nextN = Circuit.GetNextN(n);
+				Matrix[Circuit.I, Circuit.J] = Matrix[nextN.Item1, nextN.Item2];
+			}
 
-        public override string ToString()
-        {
-            var sb = new StringBuilder();
-            for (var i = 0; i < Matrix.GetLength(0); i++)
-            {
-                var sbLine = new StringBuilder();
-                for (var j = 0; j < Matrix.GetLength(1); j++)
-                {
-                    sbLine.Append(Circuit.ContainsPoint(i, j) ? $"*{Matrix[i, j],-15:N2}" : $" {Matrix[i, j],-15:N2}");
-                }
-                sb.AppendLine(sbLine.ToString());
-            }
-            return sb.ToString();
-        }
+			for (var i = Circuit.Lenth - n; i < Circuit.Lenth; i++, Circuit.MoveForward())
+			{
+				Matrix[Circuit.I, Circuit.J] = remember[i - (Circuit.Lenth - n)];
+			}
+			Circuit.Reset();
+		}
 
-        private static double Function(int i, int j)
-        {
-            return (.01 * j * j - .53 * i + 5.81) * Math.Sin((j + .8 * i * i) / 29);
-        }
+		public void Sort()
+		{
+			bool wasSwap;
+			do
+			{
+				wasSwap = false;
+				for (var i = 0; i < Circuit.Lenth - 1; i++, Circuit.MoveForward())
+				{
+					var next = Circuit.GetNextN(1);
+					if (Matrix[Circuit.I, Circuit.J] < Matrix[next.Item1, next.Item2])
+					{
+						var tmp = Matrix[Circuit.I, Circuit.J];
+						Matrix[Circuit.I, Circuit.J] = Matrix[next.Item1, next.Item2];
+						Matrix[next.Item1, next.Item2] = tmp;
 
-        private void FillMatrix()
-        {
-            for (var i = 0; i < Matrix.GetLength(0); i++)
-            {
-                for (var j = 0; j < Matrix.GetLength(1); j++)
-                    Matrix[i, j] = Function(i, j);
-            }
-        }
-    }
+						wasSwap = true;
+					}
+				}
+				Circuit.Reset();
+			} while (wasSwap);
+		}
+
+		public override string ToString()
+		{
+			var sb = new StringBuilder();
+			for (var i = 0; i < Matrix.GetLength(0); i++)
+			{
+				var sbLine = new StringBuilder();
+				for (var j = 0; j < Matrix.GetLength(1); j++)
+				{
+					sbLine.Append(Circuit.ContainsPoint(i, j) ? $"*{Matrix[i, j],-15:N2}" : $" {Matrix[i, j],-15:N2}");
+				}
+				sb.AppendLine(sbLine.ToString());
+			}
+			return sb.ToString();
+		}
+
+		private static double Function(int i, int j)
+		{
+			return (i + 1) * Math.Sin(1 / (double)(i + 1)) +
+				   (j + 1) * Math.Cos((i + 1 + 3.14 * (j + 1)) / (j + 1));
+		}
+
+		private void FillMatrix()
+		{
+			for (var i = 0; i < Matrix.GetLength(0); i++)
+			{
+				for (var j = 0; j < Matrix.GetLength(1); j++)
+					Matrix[i, j] = Function(i, j);
+			}
+		}
+	}
 }
